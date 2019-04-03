36c49f990731bbb6dec3a3dc7b92d6132335fd7a
MujahidKhan07 <mujahid@bothofus.se>
2019-04-03 19:47:26 +0500

    Initial commit

---
+++ app/src/main/java/com/madeinpk/phpfirebasepushnotification/Constants.java
@@ -0,0 +1,9 @@
+package com.madeinpk.phpfirebasepushnotification;
+
+
+public class Constants {
+    public static final String URL_REGISTER_DEVICE = "http://192.168.10.12/FcmExample/FcmExample/RegisterDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "http://192.168.10.12/FcmExample/FcmExample/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "http://192.168.10.12/FcmExample/FcmExample/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = "http://192.168.10.12/FcmExample/FcmExample/GetRegisteredDevices.php";
+}
